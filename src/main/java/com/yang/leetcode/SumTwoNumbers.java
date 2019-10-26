4929daebdc64c11015949bc491bbc42f52d2dcad
admin <794932984@qq.com>
2019-10-26 22:27:14 +0800

    LeetCode 计算两个数字之和

--- src/main/java/com/yang/leetcode/SumTwoNumbers.java
+++ src/main/java/com/yang/leetcode/SumTwoNumbers.java
@@ -1,6 +1,8 @@
 package com.yang.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 给定一个整数数组 nums 和一个目标值 target，
@@ -16,22 +18,59 @@ public class SumTwoNumbers {
         int[] nums = {2, 7, 11, 15};
         //目标数
         int target = 9;
-        int[] result = getTwoNumber(nums,target);
-        System.out.println(Arrays.toString(result));
+        //int[] result = getTwoNumber(nums, target);
+        //System.out.println(Arrays.toString(result));
+        int[] numbers = towNumber(nums, target);
+        System.out.println(Arrays.toString(numbers));
     }
 
     /**
-     * 返回目标数组下标
+     * 方法一:返回目标数组下标
+     *
      * @param nums
      * @param target
      * @return
      */
     private static int[] getTwoNumber(int[] nums, int target) {
-        for (int i =0;i<nums.length;i++){
-            for (int j= i+1;j<nums.length;j++){
-                if (nums[j] == target - nums[i]){
-                    return new int[]{i,j};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
                 }
+            }
+        }
+        throw new RuntimeException("查询不到数据");
+    }
+
+    /**
+     * 方法二
+     *
+     * @return
+     */
+    private static int[] towNumber(int[] nums, int target) {
+        //定义Map集合保存数组中的数据
+        Map<Integer, Integer> map = new HashMap<>();
+        //循环数据
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i],i);
+        }
+        //循环map集合
+        for (Map.Entry<Integer,Integer> entry:map.entrySet()){
+            //输出key
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println(key+"\t"+value);
+            //key   value
+            //2	     0
+            //7	     1
+            //11	 2
+            //15	 3
+        }
+        //开始计算
+        for (int i = 0;i<nums.length;i++){
+            int objective = target - nums[i];
+            if (map.containsKey(objective) && map.get(objective)!=i){
+                return new int[]{i,map.get(objective)};
             }
         }
         throw new RuntimeException("查询不到数据");
